877e8fb478d8725f02878660c29492afd6639407
ManuelBentjerodt <mbentjerodt@miuandes.cl>
2023-04-28 00:19:05 -0400

    styling code

--- util/program.py
+++ util/program.py
@@ -1,12 +1,8 @@
 from util.dispenser import Dispenser
 from machine import Pin
 
-def button1_handler(pin, lcd):
-    lcd.print("Hola mundo", wait=1, clear=True)
-    print("Hola mundo")
 
 class Program:
-
     def __init__(self):
         self.dispenser = Dispenser()
 
